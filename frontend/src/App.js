063aa300e81e69f74cfb0ff56e504df0813317ee
Roman Parazyan <romaparazyan@gmail.com>
2024-02-06 03:30:39 +0300

    Added BookForm, BookList and Filter components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
+import BookList from './components/BookList/BookList'
+import Filter from './components/Filter/Filter'
 import './App.css';
+import BookForm from './components/BookForm/BookForm';
+
+
 
 function App() {
   return (
@@ -6,6 +11,15 @@ function App() {
       <header className="app-header">
         <h1>Book Library App</h1>
       </header>
+      <main className='app-main'>
+        <div className='app-left-column'>
+          <BookForm/>
+        </div>
+        <div className='app-right-column'>
+          {/* <Filter/> */}
+          <BookList/>
+        </div>
+      </main>
     </div>
   );
 }
