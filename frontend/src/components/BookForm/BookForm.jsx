53695447b889de1f2172fe1da5d522d0dd75ded1
Roman Parazyan <romaparazyan@gmail.com>
2024-02-06 05:37:06 +0300

    Initialize Redux, add and display books

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,16 +1,22 @@
+import { useDispatch } from "react-redux"
 import { useState } from "react"
+import { addBook } from "../../redux/books/actionCreators"
 import './BookForm.css'
 
 
 const BookForm = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
+  const dispatch = useDispatch()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (title && author) {
-      //dispatch
-
+      const book = {
+        title,
+        author
+      }
+      dispatch(addBook(book))
       setTitle('')
       setAuthor('')
     }
@@ -21,14 +27,14 @@ const BookForm = () => {
         <h2>Add new Book</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="title">title</label>
+            <label htmlFor="title">Title:</label>
             <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
           </div>
           <div>
-            <label htmlFor="title">title</label>
+            <label htmlFor="title">Author:</label>
             <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)}/>
           </div>
-          <button type="submit">App Book</button>
+          <button type="submit">Add Book</button>
         </form>
     </div>
   )
--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,9 +1,25 @@
 import './BookList.css'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
 
 const BookList = () => {
+  const books = useSelector((state) => state.books)
   return (
     <div className="app-block book-list">
         <h2>Book List</h2>
+        {books.length === 0 ? 
+          (<p>No Books Available</p>) 
+          : (
+            <ul>
+              {books.map((book, i) => (
+                <li key={i}>
+                  <div className="book-info">
+                      {++i}. {book.title} by <strong>{book.author}</strong>
+                    </div>
+                </li>
+              ))}
+            </ul>
+          )
+        }
     </div>
   )
 }
